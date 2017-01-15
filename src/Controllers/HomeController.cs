b8492a03fbc41d7921e0b20ec2b9e126c60af5f1
xferra <xferra@live.com>
2017-01-15 15:56:36 +0300

    Try to update status

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
         [HttpPost]
-        public IActionResult Webhook([FromBody] string data)
+        public async Task<IActionResult> Webhook([FromBody] dynamic data)
         {
-            return Json(data);
+            var statusUrl = data?.pull_request?.statuses_url;
+            if (statusUrl != null) 
+            {
+                var client = new HttpClient();
+                var content = "{state: \"success\", target_url: \"http://repometric.com\", description: \"Hello Integration\", context: \"Repometric\"}";
+                var contentPost = new StringContent(content, System.Text.Encoding.UTF8, "application/json");
+                await client.PostAsync(statusUrl, contentPost);
+            }
+            return Json(statusUrl);
         }
 
         public IActionResult Index()
